b52a9d227dce5360c54c69ff66eb82bfa4b09f10
Andrey Klimenko <klimandr@mail.ru>
2018-02-08 14:56:14 +0300

    Edited common and frontend main.php. And main-local too actually.

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,11 @@
 <?php
 return [
+    'name' => 'stolennews',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'language' => 'ru-RU',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -9,6 +9,7 @@ $params = array_merge(
 return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
+    'defaultRoute' => 'site',//можно заменить, чтобы индекс направлял в другое место
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
